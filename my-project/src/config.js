fe4a6e94d09bbddb2c90886b98f9525edce375a1
Harshita Jain <harshitajain5757@gmail.com>
2026-03-07 14:35:39 +0530

    Change

--- my-project/src/config.js
+++ my-project/src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
 // Replace the IP address with your NEW Vercel URL
-export const API_BASE_URL = "https://access-learn-backend-gxrm.vercel.app/api";
+export const API_BASE_URL = "https://access-learn-backend-git-main-harshita2806s-projects.vercel.app/api";
